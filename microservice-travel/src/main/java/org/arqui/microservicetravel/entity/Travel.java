b25228fb4e4e336c78b89c455bfe8ab3bbd0e656
Tomas Ferrante <tomasferrante@live.com>
2025-10-30 18:25:42 -0300

    Travel

---
+++ microservice-travel/src/main/java/org/arqui/microservicetravel/entity/Travel.java
@@ -0,0 +1,36 @@
+package org.arqui.microservicetravel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Travel {
+    @Id
+    private Long id_travel;
+    @Column
+    private Date fechaInicio;
+    @Column
+    private Date fechaFin;
+    @Column
+    private LocalTime horaInicio;
+    @Column
+    private LocalTime horaFin;
+    @Column
+    private Integer kmRecorridos;
+    @Column
+    private Boolean pausado;
+    @Column
+    private User usuario;
+    @Column
+    private Stop parada;
+}
