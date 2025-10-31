04a9b5cafd36c0001ff71309775c25669f586b76
Elisa Guaspari <eliguaspari@gmail.com>
2025-10-31 09:12:20 -0300

    Anotaciones solucionado

--- microservice-user/src/main/java/org/arqui/microserviceuser/repository/AccountRepository.java
+++ microservice-user/src/main/java/org/arqui/microserviceuser/repository/AccountRepository.java
@@ -1,7 +1,9 @@
 package org.arqui.microserviceuser.repository;
 
 import org.arqui.microserviceuser.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<Account,Long>{
+public interface AccountRepository extends JpaRepository<Account,Long> {
 }
--- microservice-user/src/main/java/org/arqui/microserviceuser/repository/UserRepository.java
+++ microservice-user/src/main/java/org/arqui/microserviceuser/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.arqui.microserviceuser.repository;
 
 import org.arqui.microserviceuser.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<User, Long> {
 }
