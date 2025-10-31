50eb02a5828a00ebe26de3f398985cb8041b6753
Tomas Abel Ferrante <tomax19cr7@gmail.com>
2025-10-30 23:38:54 -0300

    User creo que terminado

--- microservice-user/src/main/java/org/arqui/microserviceuser/entity/User.java
+++ microservice-user/src/main/java/org/arqui/microserviceuser/entity/User.java
@@ -1,30 +1,34 @@
 package org.arqui.microserviceuser.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.arqui.microserviceuser.Rol;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
 import java.util.List;
 
 
 @Entity
 @Data
-@NoArgConstructor
+@NoArgsConstructor
 @AllArgsConstructor
 public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id_user;
     @Column
+    // El profe no lo puso
     private String nombreUsuario;
     @Column
     private String nombre;
     @Column
+    // El profe usar nombre para ambos
     private String apellido;
     @Column
     private String email;
     @Column
+    // El profe no lo puso
     private Integer numeroCelular;
     @Column
     private Rol rol;
