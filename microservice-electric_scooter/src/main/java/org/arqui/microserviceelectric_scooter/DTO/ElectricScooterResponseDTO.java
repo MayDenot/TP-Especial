577ad17b23d1b57c8afb43f5dfa6af6014253b6b
Valentin <valenfacu316@gmail.com>
2025-11-03 17:15:39 -0300

    agego mapeador con string

--- microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/DTO/ElectricScooterResponseDTO.java
+++ microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/DTO/ElectricScooterResponseDTO.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ElectricScooterResponseDTO {
-    private Long id;
+    private String id;
     private Double longitud;
     private Double latitud;
     private Boolean habilitado;
