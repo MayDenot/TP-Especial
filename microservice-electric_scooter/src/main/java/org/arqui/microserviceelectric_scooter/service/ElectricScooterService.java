9f21ba1b8452649d76cab2d4bbe0326767edee42
Valentin <valenfacu316@gmail.com>
2025-11-03 18:10:41 -0300

    agrego save

--- microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/service/ElectricScooterService.java
+++ microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/service/ElectricScooterService.java
@@ -1,4 +1,22 @@
 package org.arqui.microserviceelectric_scooter.service;
 
+import org.arqui.microserviceelectric_scooter.entity.ElectricScooter;
+import org.arqui.microserviceelectric_scooter.mapeador.ElectricScooterMapper;
+import org.arqui.microserviceelectric_scooter.repository.ElectricScooterRepository;
+import org.arqui.microserviceelectric_scooter.service.DTO.Request.ElectricScooterRequestDTO;
+
 public class ElectricScooterService {
+
+
+    private ElectricScooterRepository repository;
+
+
+    public ElectricScooterService(ElectricScooterRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(ElectricScooterRequestDTO electricScooter) {
+        ElectricScooter entity = ElectricScooterMapper.toEntity(electricScooter);
+        repository.save(entity);
+    }
 }
