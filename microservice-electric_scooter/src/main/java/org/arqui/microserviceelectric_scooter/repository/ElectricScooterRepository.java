49c052c330b22de2b62f2ad8e8c4a002955adf4c
Valentin <valenfacu316@gmail.com>
2025-11-03 17:13:46 -0300

    agego mapeador

--- microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/repository/ElectricScooterRepository.java
+++ microservice-electric_scooter/src/main/java/org/arqui/microserviceelectric_scooter/repository/ElectricScooterRepository.java
@@ -1,7 +1,16 @@
 package org.arqui.microserviceelectric_scooter.repository;
 
+import org.arqui.microserviceelectric_scooter.DTO.ElectricScooterRequestDTO;
 import org.arqui.microserviceelectric_scooter.entity.ElectricScooter;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 
-public interface ElectricScooterRepository extends MongoRepository<ElectricScooter, String> {
+public interface ElectricScooterRepository extends MongoRepository<ElectricScooterRepository, String> {
+
+    void deleteById(String id);
+    void save(ElectricScooterRequestDTO electricScooter);
+
+
+
+
 }
