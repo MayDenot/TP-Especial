cd38be034bdbdca93f98630e5c20c9eeab8ba0d2
Mayra Denot <mayra.denot@gmail.com>
2025-11-03 15:41:20 -0300

    Microservicio admin

---
+++ microservice-admin/src/main/java/org/arqui/microserviceadmin/MicroserviceAdminApplication.java
@@ -0,0 +1,13 @@
+package org.arqui.microserviceadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroserviceAdminApplication.class, args);
+  }
+
+}
--- microservice-maintenance/src/main/java/org/arqui/microservicemaintenance/State.java
+++
@@ -1,9 +0,0 @@
-package org.arqui.microservicemaintenance;
-
-public enum State {
-  PENDIENTE_REVISION,
-  EN_REVISION,
-  EN_REPARACION,
-  ESPERANDO_REPUESTOS,
-  REPARADO,
-}
