1e039c821dea7ad79ee50f9d24ae9b35bc6b03c3
Elisa Guaspari <eliguaspari@gmail.com>
2025-11-06 16:46:18 -0300

    nombre clients

--- microservice-user/src/main/java/org/arqui/microserviceuser/feignClients/TravelClients.java
+++ microservice-user/src/main/java/org/arqui/microserviceuser/feignClients/TravelClients.java
@@ -1,5 +1,17 @@
 package org.arqui.microserviceuser.feignClients;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@FeignClient(name = "microservice-travel", url = "http://localhost:8084")
 public interface TravelClients {
+    @GetMapping("/api/travel/fechaInicio/{inicio}/fechaFin/{fin}")
+    public List<Long> obtenerIdUsuariosConMasViajes(
+            @PathVariable LocalDate fechaInicio,
+            @PathVariable LocalDate fechaFin);
 
 }
