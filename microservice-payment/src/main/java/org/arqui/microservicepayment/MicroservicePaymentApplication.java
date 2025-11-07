efe5cadd9518746c712846bc18db28b9db212c74
Mayra Denot <mayra.denot@gmail.com>
2025-11-07 16:41:35 -0300

    Faltaba anotacion de feign client

--- microservice-payment/src/main/java/org/arqui/microservicepayment/MicroservicePaymentApplication.java
+++ microservice-payment/src/main/java/org/arqui/microservicepayment/MicroservicePaymentApplication.java
@@ -2,8 +2,10 @@ package org.arqui.microservicepayment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroservicePaymentApplication {
 
   public static void main(String[] args) {
